aafb880839a272e469012e2a30bafc77c33c1b84
GozdeUgurK <147741168+GozdeUgurK@users.noreply.github.com>
2023-11-14 22:01:18 +0300

    upd

--- Gozde_calisankod.R
+++ Gozde_calisankod.R
@@ -5,21 +5,7 @@ install.packages("remotes")
 library(remotes)
 
 install_github('vincentarelbundock/WDI')
-#DENEME1
-country_code <- "TR"
-indicator_code <- "SP.POP.TOTL.FE.IN"
-start_date <- 2018
-end_date <- 2023
 
-world_bank_data <- WDI( indicator = indicator_code,
-                        country = country_code,
-                        start = start_date, 
-                        end = end_date, 
-                        extra = TRUE)
-
-saveRDS(world_bank_data, file = "world_bank_data.rds")
-loaded_data <- readRDS("world_bank_data.rds")
-loaded_data
 
 #DENEME2
 
@@ -47,15 +33,32 @@ loaded_data
 
 #"FP.CPI.TOTL.ZG" --ENFLASYON
 
-"SL.EMP.TOTL.SP.ZS" : Employment to Population Ratio:
+"SL.EMP.TOTL.SP.ZS" : #Employment to Population Ratio:
   
   "SP.POP.TOTL.WB.AG25-54"
 
-Female Labor Force Participation Rate:
-  Indicator Code: SL.TLF.CACT.FE.ZS
-Male Labor Force Participation Rate:
-  Indicator Code: SL.TLF.CACT.MA.ZS
-Female Unemployment Rate:
-  Indicator Code: SL.UEM.TOTL.FE.ZS
-Male Unemployment Rate:
-  Indicator Code: SL.UEM.TOTL.MA.ZS
+# Female Labor Force Participation Rate:
+#   Indicator Code: SL.TLF.CACT.FE.ZS
+# Male Labor Force Participation Rate:
+#   Indicator Code: SL.TLF.CACT.MA.ZS
+# Female Unemployment Rate:
+#   Indicator Code: SL.UEM.TOTL.FE.ZS
+# Male Unemployment Rate:
+#   Indicator Code: SL.UEM.TOTL.MA.ZS
+
+
+#DENEME1
+# country_code <- "TR"
+# indicator_code <- "SP.POP.TOTL.FE.IN"
+# start_date <- 2018
+# end_date <- 2023
+# 
+# world_bank_data <- WDI( indicator = indicator_code,
+#                         country = country_code,
+#                         start = start_date, 
+#                         end = end_date, 
+#                         extra = TRUE)
+# 
+# saveRDS(world_bank_data, file = "world_bank_data.rds")
+# loaded_data <- readRDS("world_bank_data.rds")
+# loaded_data
